5d175daf852e702cfc49fd7fd5520b9febad3293
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-01-05 20:37:05 +0800

    Create SQL.sql

---
+++ SQL/SQL.sql
@@ -0,0 +1,22 @@
+SQL: Structured Query Language 
+  结构化查询语言，包含 DQL，DML，DDL，DCL 四种语言
+
+DQL: Data Query Language 
+    select
+  
+  
+DML: Data Manipulation Language
+    insert
+    delete 
+    update
+
+DDL: Data Definition Language
+    create
+
+DCL: Data Control Language
+    grand
+    rollback
+    commit：
+        显示提交：SQL> commit;
+        隐式提交：ALTER，AUDIT，COMMENT，CONNECT，CREATE，DISCONNECT，DROP，EXIT，GRANT，NOAUDIT，QUIT，REVOKE，RENAME
+        自动提交：SQL> set autocommit on;
