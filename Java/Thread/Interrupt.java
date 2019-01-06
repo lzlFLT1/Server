4199546defe7c952deb810ff793534336e1d2bf9
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-01-07 00:01:57 +0800

    Update Interrupt.java

--- Java/Thread/Interrupt.java
+++ Java/Thread/Interrupt.java
@@ -2,21 +2,21 @@ package kasei.demo;
 
 public class Interrupt implements Runnable {
 
-    long i=0L;
+    long i = 0L;
 
     @Override
     public void run() {
         try {
-            while (!Thread.currentThread().isInterrupted() && i<100000000L) {
+            while (!Thread.currentThread().isInterrupted() && i < 100000000L) {
                 // 如果当前线程没有被其他线程打断
                 System.out.println(i++);
-                Thread.sleep(1000);
+                //Thread.sleep(1000); // 只有在线程处于阻塞状态时被 interrupt 才会抛 InterruptedException 异常，可以选择注释掉该行进行对比
             }
 
             System.out.println("正常退出");
-        } catch (Exception e) {
+        } catch (Exception e) { // 该异常会清除 interrupt 标记
             System.out.println("被打断了");
-        }
+        } 
 
     }
 
