70dd4824daf41ef565ae9dd76dd4808e26f2232a
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-02-21 15:23:28 +0800

    Update package-info.java

--- Java/Syntax/exception/package-info.java
+++ Java/Syntax/exception/package-info.java
@@ -1,6 +1,6 @@
 package kasei.javase.se.syntax.exception;
 
-/* Java 异常层次结构:********************************************************
+/** Java 异常层次结构:********************************************************
  * Throwable
  *  │
  *  ├─ Error                            (Error 类及其子类，程序员无法处理，也不必处理)
@@ -8,3 +8,9 @@ package kasei.javase.se.syntax.exception;
  *        ├─ Exception及其子类           (Checked类异常，必须处理，外部错误，比如文件读取失败、网络连接错误等)
  *        └─ RuntimeException及其子类    (Unchecked类异常，处不处理均可，如果出现该类型异常，那么一定是程序员代码写错了)
  * */
+
+/** Java 异常抛出原则
+ * 1. 调用函数 需要 知道 被调函数 的执行状态：被调函数 应该抛出 Checked 异常
+ * 2. 如果被调函数碰到了不可预知的异常，那么应该抛出 Unchecked (Error 或 RuntimeException)异常
+ * */
+
