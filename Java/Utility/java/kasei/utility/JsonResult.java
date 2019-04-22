cb2022604842abf3932626d523ba81e4c59dd4de
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-04-22 14:43:38 +0800

    Update JsonResult.java

--- Java/Utility/java/kasei/utility/JsonResult.java
+++ Java/Utility/java/kasei/utility/JsonResult.java
@@ -1,5 +1,24 @@
 public class JsonResult<T>{
-    public int code;
-    public String msg;
-    public T data;
+    private int code;
+    private String msg;
+    private T data;
+
+
+    public JsonResult(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
